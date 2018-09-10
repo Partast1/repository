63f0ecc5473770789a0c7a6976dde3be05df0625
Anders Møller <andersa1989@gmail.com>
2018-09-10 15:49:05 +0200

    TimeToGoHome

---
+++ Methods-files/Methods-files/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Methods_files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Creates one Folder
+            //Directory.CreateDirectory(@".\Droids");
+            ////Creates two subfolders
+            //Directory.CreateDirectory(@".\Droids\Astromech");
+            //Directory.CreateDirectory(@".\Droids\Protocol");
+            ////Creates two text files with some text
+            //File.WriteAllText(@".\Droids\Astromech\r2d2.text", "beep beep");
+            //File.WriteAllText(@".\Droids\Protocol\C3p0.text", "Sir");
+            ////displays all files
+            //foreach (string file in Directory.GetFiles(@".\"))
+            //{
+            //    Console.WriteLine(file);
+            //}
+            //Console.Read();
+
+            using (var writer = new StreamWriter(
+                new FileStream(@".\StarWars.txt", FileMode.Create)))
+            {
+                writer.Write("Beep boop!");
+            }
+            using (var reader = new StreamReader (
+                new FileStream(@".\StarWars.txt", FileMode.Open)))
+            {
+                Console.WriteLine(reader.ReadToEnd());
+            }
+            Console.ReadLine();
+        }
+    }
+}
