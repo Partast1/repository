71b6021f1830fcfde9b4365b72e7ed462a73687d
Partast1 <43130360+Partast1@users.noreply.github.com>
2018-09-26 13:59:51 +0200

    Revert "a"
    This reverts commit 0d6bf45a21ecfb556ef8fc7e2633afe3c6b68a62.

---
+++ CV/CV/Data/CVContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CV.Models
+{
+    public class CVContext : DbContext
+    {
+        public CVContext (DbContextOptions<CVContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CV.Models.Movies> Movies { get; set; }
+    }
+}
